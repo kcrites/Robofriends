583ae0a859def27c2a43d4decdbd93f08e5fcfa7
Ken Crites <kenrcrites@gmail.com>
2018-11-27 22:44:29 +0100

    gh-pages added

---
+++ precache-manifest.3041a317ee74f5bcf68e8abe8feb4bc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "39a3a35c71e0df973673",
+    "url": "/Robofriends/static/js/runtime~main.39a3a35c.js"
+  },
+  {
+    "revision": "42ce95fb6d704af3ab83",
+    "url": "/Robofriends/static/js/main.42ce95fb.chunk.js"
+  },
+  {
+    "revision": "7538553cd97392dcd7a3",
+    "url": "/Robofriends/static/js/1.7538553c.chunk.js"
+  },
+  {
+    "revision": "42ce95fb6d704af3ab83",
+    "url": "/Robofriends/static/css/main.221a8cb5.chunk.css"
+  },
+  {
+    "revision": "7538553cd97392dcd7a3",
+    "url": "/Robofriends/static/css/1.8397ce4c.chunk.css"
+  },
+  {
+    "revision": "fdb192913486ac26c82b42d440aa2d9e",
+    "url": "/Robofriends/index.html"
+  }
+];
