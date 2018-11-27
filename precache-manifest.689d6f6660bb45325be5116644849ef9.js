9d67fcc2545e03e6885fc69c55c4cfc16714628d
Ken Crites <kenrcrites@gmail.com>
2018-11-27 09:19:50 +0100

    First version

---
+++ precache-manifest.689d6f6660bb45325be5116644849ef9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f62c3851e5c3bb91686",
+    "url": "/static/js/main.6f62c385.chunk.js"
+  },
+  {
+    "revision": "7538553cd97392dcd7a3",
+    "url": "/static/js/1.7538553c.chunk.js"
+  },
+  {
+    "revision": "6f62c3851e5c3bb91686",
+    "url": "/static/css/main.a78d7f61.chunk.css"
+  },
+  {
+    "revision": "7538553cd97392dcd7a3",
+    "url": "/static/css/1.8397ce4c.chunk.css"
+  },
+  {
+    "revision": "cfe3b1b0a87e5604125e11b55cc968d9",
+    "url": "/index.html"
+  }
+];
